b6f422942108970c5aa09831954b2a61f52ccbff
Daniel Nicolai <dalanicolai@gmail.com>
2022-05-25 11:47:16 +0200

    Update evilified-state-evilify's to evilified-state-evilify-map's

--- packages.el
+++ packages.el
@@ -82,7 +82,9 @@
       (evil-define-key 'visual pdf-view-mode-map "y" 'pdf-view-kill-ring-save)
 
       ;; TODO: Make `/', `?' and `n' work like in Evil
-      (evilified-state-evilify pdf-view-mode pdf-view-mode-map
+      (evilified-state-evilify-map pdf-view-mode-map
+        :mode  pdf-view-mode
+        :bindings
         ;; Navigation
         "0"  'image-bol
         "$"  'image-eol
@@ -113,51 +115,47 @@
         "o"   'pdf-links-action-perform
         "O"   'pdf-outline
         "zr"  'pdf-view-scale-reset)
-
-      (with-eval-after-load 'pdf-outline
-        (evilified-state-evilify-map pdf-outline-buffer-mode-map
-          :mode  pdf-outline-buffer-mode
-          :bindings
-          "-"                'negative-argument
-          "j"                'next-line
-          "k"                'previous-line
-          "gk"               'outline-backward-same-level
-          "gj"               'outline-forward-same-level
-          (kbd "<backtab>")  (if (version< emacs-version "28.0")
-                                 'outline-show-all
-                               'outline-cycle-buffer)
-          "gh"               'pdf-outline-up-heading
-          "gg"               'beginning-of-buffer
-          "G"                'pdf-outline-end-of-buffer
-          (kbd "<tab>")      'outline-toggle-children
-          "RET"              'pdf-outline-follow-link
-          (kbd "M-RET")      'pdf-outline-follow-link-and-quit
-          "f"                'pdf-outline-display-link
-          [mouse-1]          'pdf-outline-mouse-display-link
-          "o"                'pdf-outline-select-pdf-window
-          "``"               'pdf-outline-move-to-current-page
-          "''"               'pdf-outline-move-to-current-page
-          "Q"                'pdf-outline-quit-and-kill
-          "q"                'quit-window
-          "F"                'pdf-outline-follow-mode))
-      (with-eval-after-load 'pdf-annot
-        (evilified-state-evilify-map pdf-annot-list-mode-map
-          :mode  pdf-annot-list-mode
-          :bindings
-          "f"                'pdf-annot-list-display-annotation-from-id
-          "d"                'tablist-flag-forward
-          "x"                'tablist-do-flagged-delete
-          "u"                'tablist-unmark-forward
-          "q"                'tablist-quit))
-      (with-eval-after-load 'pdf-occur
-        (evilified-state-evilify-map pdf-occur-buffer-mode-map
-          :mode  pdf-occur-buffer-mode
-          :bindings
-          "q"              'tablist-quit
-          "g"              'pdf-occur-revert-buffer-with-args
-          "r"              'pdf-occur-revert-buffer-with-args
-          "*"              'spacemacs/enter-ahs-forward
-          "?"              'evil-search-backward))
+      (evilified-state-evilify-map pdf-outline-buffer-mode-map
+        :mode  pdf-outline-buffer-mode
+        :bindings
+        "-"                'negative-argument
+        "j"                'next-line
+        "k"                'previous-line
+        "gk"               'outline-backward-same-level
+        "gj"               'outline-forward-same-level
+        (kbd "<backtab>")  (if (version< emacs-version "28.0")
+                               'outline-show-all
+                             'outline-cycle-buffer)
+        "gh"               'pdf-outline-up-heading
+        "gg"               'beginning-of-buffer
+        "G"                'pdf-outline-end-of-buffer
+        (kbd "<tab>")      'outline-toggle-children
+        "RET"              'pdf-outline-follow-link
+        (kbd "M-RET")      'pdf-outline-follow-link-and-quit
+        "f"                'pdf-outline-display-link
+        [mouse-1]          'pdf-outline-mouse-display-link
+        "o"                'pdf-outline-select-pdf-window
+        "``"               'pdf-outline-move-to-current-page
+        "''"               'pdf-outline-move-to-current-page
+        "Q"                'pdf-outline-quit-and-kill
+        "q"                'quit-window
+        "F"                'pdf-outline-follow-mode)
+      (evilified-state-evilify-map pdf-annot-list-mode-map
+        :mode  pdf-annot-list-mode
+        :bindings
+        "f"                'pdf-annot-list-display-annotation-from-id
+        "d"                'tablist-flag-forward
+        "x"                'tablist-do-flagged-delete
+        "u"                'tablist-unmark-forward
+        "q"                'tablist-quit)
+      (evilified-state-evilify-map pdf-occur-buffer-mode-map
+        :mode  pdf-occur-buffer-mode
+        :bindings
+        "q"              'tablist-quit
+        "g"              'pdf-occur-revert-buffer-with-args
+        "r"              'pdf-occur-revert-buffer-with-args
+        "*"              'spacemacs/enter-ahs-forward
+        "?"              'evil-search-backward)
       (spacemacs/declare-prefix-for-mode 'pdf-occur-buffer-mode "mt" "toggles")
       (spacemacs/set-leader-keys-for-major-mode 'pdf-occur-buffer-mode
         "tf" 'next-error-follow-minor-mode))))
